7ce474148190087fcc8fb0c8534cc2cb9635effe
Nikita <nikon12344@gmail.com>
2023-05-14 17:51:26 +0400

    oooooooooooo

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,24 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+
+        List<String> list = new ArrayList<>();
+
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            for(String element : entry.getValue()){
+                if (element.equals(developer)) {
+                    list.add(entry.getKey());
+                }
+            }
+        }
+
+        list.sort(Comparator.comparingInt(String::length));
+        Collections.reverse(list);
+
+        return list;
     }
 }
+
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,28 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+
+        Map<Integer, Set<String>> map = new HashMap<>();
+
+        for (Map.Entry<String, Integer> entry : sourceMap.entrySet()) {
+
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+
+            if (!map.containsKey(key.length())) {
+                Set<String> set = new HashSet<>();
+                set.add(key);
+                for (Map.Entry<String, Integer> entry2 : sourceMap.entrySet()) {
+                    if(!entry2.getValue().equals(value) && entry2.getKey().length() == key.length()){
+                        set.add(entry2.getKey());
+                    }
+                }
+                map.put(key.length(), set);
+            }
+        }
+        return map;
     }
 }
